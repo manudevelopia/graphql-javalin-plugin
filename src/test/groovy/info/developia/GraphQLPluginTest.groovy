c8f2da7d9637a561f123567ca385279d522aa180
manudevelopia <manu@developia.info>
2025-07-24 16:45:42 +0200

    fix basic unit test

--- src/test/groovy/info/developia/GraphQLPluginTest.groovy
+++ src/test/groovy/info/developia/GraphQLPluginTest.groovy
@@ -1,21 +1,32 @@
 package info.developia
 
+import graphql.schema.DataFetcher
 import io.javalin.Javalin
 import spock.lang.Specification
 
 class GraphQLPluginTest extends Specification {
 
     def "application has a greeting"() {
-
+        given:
+        var graphQLOptions = GraphQLOptions.builder()
+                .addPackage('io.javalin.examples')
+                .registerQuery('hello', sayHello)
+                .registerQuery('bye', sayBye).build()
         when:
-        Javalin.create(config -> {
-            config.registerPlugin(new GraphQLPlugin((userConfig) -> {
-                userConfig.limit = 10
-                userConfig.limites = 10
-            }))
+        var server = Javalin.create(config -> {
+            config.registerPlugin(new GraphQLPlugin(graphQLOptions))
         }).start(7000)
-
         then:
-        result != null
+        noExceptionThrown()
+        cleanup:
+        server.stop()
+    }
+
+    private final DataFetcher<String> sayHello = ctx -> {
+        return "Hello ${ctx.getArgument('name')}"
+    }
+
+    private final DataFetcher<String> sayBye = ctx -> {
+        return "Bye-bye ${ctx.getArgument('name')}"
     }
 }
